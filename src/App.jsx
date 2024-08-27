534292ffd49337639a4fcd11f9a8954f9c2d6cc5
andyhdang <andyhd0614@gmail.com>
2024-08-27 12:27:40 -0700

    another update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <h1>This is a change</h1>
       <p>this is another change</p>
+      <p>and another</p>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
